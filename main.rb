f5c0a2c357af0e3115c9be6152e0aee02ce84914
AJ Norton <ajn123@vt.edu>
2014-05-24 03:50:32 -0400

    added changes

--- main.rb
+++ main.rb
@@ -2,12 +2,30 @@ require 'rubygems'
 require 'mechanize'
 require_relative 'parse'
 
-agent = Mechanize.new
 
 
+class DropAddNavigator
+  include NavigateHokieSpa
 
-form = hokie_spa_welcome({agent: agent})
-page = login({form: form,agent: agent})
-page =traverse_to_schedule(page_and_agent(page,agent))
-terms_available(page_and_agent(page,agent))
-drop_add_links({page: page})
+  attr_accessor :form,:page,:agent
+
+  def initialize
+    @agent = Mechanize.new
+
+  end
+
+  def page_and_agent
+    {page: @page, agent: @agent}
+  end
+
+  def navigate_to_drop_add
+    @form = hokie_spa_welcome_page(page_and_agent)
+    @page = login(page_and_agent.merge({form: form}))
+    @page = traverse_to_schedule(page_and_agent)
+    terms_available(page_and_agent)
+    drop_add_links(page_and_agent)
+  end
+end
+
+a = DropAddNavigator.new
+a.navigate_to_drop_add
--- parse.rb
+++ parse.rb
@@ -1,50 +1,59 @@
 
-def login(args = {})
-  puts "What is your username?"
-  args[:form].username=gets.chomp
-  puts "What is your password?"
-  args[:form].password = gets.chomp
-  args[:agent].submit(args[:form], args[:form].buttons.first)
-end
 
-def hokie_spa_welcome(args = {})
-  agent = args[:agent]
-  page = agent.get('http://hokiespa.vt.edu')
-  page = agent.page.link_with(:text => 'Login to HokieSpa >>>').click
-  page.forms()[0]
-end
+module NavigateHokieSpa
 
 
 
-#Find registration and schedule page.
-def traverse_to_schedule(args = {})
-  page = args[:page]
-  agent = args[:agent]
-  text_link =  page.links.map{|link| link.text.include?"Not Now"}
-  page = agent.page.link_with(:text =>  "\n      Not Now\n    ").click if text_link.include? true
-  page = agent.page.link_with(:text =>  "Hokie Spa").click if text_link.include? true
-  page = agent.page.link_with(:text => "Registration and Schedule").click
-  page
-end
+  def hokie_spa_welcome_page(args = {})
+    agent = args[:agent]
+    page = agent.get('http://hokiespa.vt.edu')
+    page = agent.page.link_with(:text => 'Login to HokieSpa >>>').click
+    page.forms()[0]
+  end
 
 
-#parse all valid terms
-def terms_available(args = {})
-  agent = args[:agent]
-  page = args[:page]
-  terms = agent.page.search('tr > td.delabel > p > b').reject{|term| term.to_s !~ /\d{4}/}
-  terms
-end
-
-def page_and_agent(page,agent)
-  {page: page, agent: agent}
-end
+  def login(args = {})
+    puts "What is your username?"
+    args[:form].username=gets.chomp
+    puts "What is your password?"
+    args[:form].password = gets.chomp
+    args[:agent].submit(args[:form], args[:form].buttons.first)
+  end
 
 
-#get all drop add links
-def drop_add_links(args = {})
-  page = args[:page]
-  drop_add = page.links.select{|link| link.text == "Drop/Add" }
-  drop_add.uniq!{|link| link.href }
-  pp drop_add
+
+
+
+  #Find registration and schedule page.
+  def traverse_to_schedule(args = {})
+    page = args[:page]
+    agent = args[:agent]
+    text_link =  page.links.map{|link| link.text.include?"Not Now"}
+    page = agent.page.link_with(:text =>  "\n      Not Now\n    ").click if text_link.include? true
+    page = agent.page.link_with(:text =>  "Hokie Spa").click if text_link.include? true
+    page = agent.page.link_with(:text => "Registration and Schedule").click
+    page
+  end
+
+
+  #parse all valid terms
+  def terms_available(args = {})
+    agent = args[:agent]
+    page = args[:page]
+    terms = agent.page.search('tr > td.delabel > p > b').reject{|term| term.to_s !~ /\d{4}/}
+    terms
+  end
+
+
+
+
+  #get all drop add links
+  def drop_add_links(args = {})
+    page = args[:page]
+    drop_add = page.links.select{|link| link.text == "Drop/Add" }
+    drop_add.uniq!{|link| link.href }
+    pp drop_add
+  end
+
+
 end
